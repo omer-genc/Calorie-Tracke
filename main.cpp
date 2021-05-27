79bc18bbb28ca987cc5067f4c24c03de2199eb66
Ömer Genç <omergnc1@gmail.com>
2021-05-27 15:21:31 +0300

    planlama

---
+++ main.cpp
@@ -0,0 +1,26 @@
+/*
+    coder: omer-genc
+    ogunler.h
+        classlar:
+            (small medium large)
+            Breakfast
+            Lunch
+            Dinner
+    
+    sporlar.h
+        claslar:
+            Basketball
+            Football
+            Tennis
+            Swimming
+    
+    users.h
+        claslar:
+            User:
+                ad,soyad,ID,yaş,kilo
+                alınan verilen kalori
+                kaçıncı hafta
+                öğün ve spor classları
+
+
+*/
