d0c23c54fb849ef0181c6b7c6d9d59530332a3cd
Ömer Genç <omergnc1@gmail.com>
2021-05-27 21:37:05 +0300

    All meal test succes

--- Meal_test.cpp
+++ Meal_test.cpp
@@ -1,6 +1,7 @@
 #include "Meals.h"
 
 int main(){
+    cout<<"Breakfast test"<<endl;
     Breakfast br1;
     cout<<br1;
     Breakfast br2(10,10);
@@ -12,5 +13,34 @@ int main(){
     Breakfast br4(20,20);
     br3 = br4 + br2;
     cout<<br3;
+    cout<<endl<<endl;
+
+    cout<<"Lunch test"<<endl;
+    Lunch ln1;
+    cout<<ln1;
+    Lunch ln2(10,10);
+    cout<<ln2;
+    Lunch ln3(ln2);
+    cout<<ln3;
+    ln3 = ln1;
+    cout<<ln3;
+    Lunch ln4(20,20);
+    ln3 = ln4 + ln2;
+    cout<<ln3;
+    cout<<endl<<endl;
+
+    cout<<"Dinner test"<<endl;
+    Dinner dn1;
+    cout<<dn1;
+    Dinner dn2(10,10);
+    cout<<dn2;
+    Dinner dn3(dn2);
+    cout<<dn3;
+    dn3 = dn1;
+    cout<<dn3;
+    Dinner dn4(20,20);
+    dn3 = dn4 + dn2;
+    cout<<dn3;
+    cout<<endl<<endl;
     return 0;
 }
